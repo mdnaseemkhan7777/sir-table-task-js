c17ba0e2b0f666b6c5696978fbba18f9962db1aa
mdnaseemkhan7777 <74352507+mdnaseemkhan7777@users.noreply.github.com>
2022-01-20 10:42:12 +0500

    main

--- app.js
+++ app.js
@@ -13,7 +13,42 @@ function add(){
     td1.innerHTML = inputName;
     td2.innerHTML= inputClass;
     td3.innerHTML= inputRoll;
-    td4.innerHTML= '<i class="fa fa-edit text" onclick="update(this)" style="font-size:36px; color:#ff4500"></i> &nbsp; <i class="fa fa-trash-o text" onclick="delet(this)" style="font-size:36px;color:#dc3545"></i>';
+    td4.innerHTML= '<i class="fa fa-edit text" onclick="update(this)" style="font-size:36px; color:#ff4500"></i> &nbsp; <i class="fa fa-trash-o text" onclick="delet(this)" style="font-size:36px;color:#dc3545"></i>'; 
+
+    if( inputClass == '1' ){
+        tr.style.background = '#343a40';
+      }
+      else if( inputClass == '2' ){
+        tr.style.background = "#1089ff";
+      }
+      else if( inputClass == '3' ){
+        tr.style.background = "#28a745";
+      }
+      else if( inputClass == '4' ){
+        tr.style.background = "#ffc107";
+      }
+      else if( inputClass == '5' ){
+        tr.style.background = "#fa9729";
+      }
+      else if( inputClass == '6' ){
+        tr.style.background = "#17a2b8";
+      }
+      else if( inputClass == '7' ){
+        tr.style.background = "#FFCA28";
+      }
+      else if( inputClass == '8' ){
+        tr.style.background = "#422E45";
+      }
+      else if( inputClass == '9' ){
+        tr.style.background = "#080808";
+      }
+      else if( inputClass == '10' ){
+        tr.style.background = "#874E1C";
+      }
+
+
+
+
     document.getElementById('tbl').appendChild(tr);
 
     function clear(){
@@ -40,7 +75,7 @@ function update(stud){
     let td4 = tr.appendChild(document.createElement('td'));
 
     td1.innerHTML = '<input type="text"placeholder="Name"  id="name1" >';
-    td2.innerHTML= '<input type="text"placeholder="Class" id="class1" >';
+    td2.innerHTML= '<input type="text"placeholder="Class"  id="class1" >';
     td3.innerHTML= '<input type="text"placeholder="Roll" id="roll1">';
     td4.innerHTML= '<i class="fa fa-check text" onclick="addupdate(this)" style="font-size:36px; color:#198754"></i> &nbsp; <i class="fa fa-close text" onclick="delet(this)" style="font-size:36px;color:#dc3545"></i>';
     document.getElementById('tbl').replaceChild(tr,s);
@@ -54,6 +89,8 @@ function addupdate(stud){
     let inputClass = document.getElementById("class1").value;
     let inputRoll = document.getElementById("roll1").value;
     var s = stud.parentNode.parentNode;
+
+    
     let tr = document.createElement('tr');
 
     let td1 = tr.appendChild(document.createElement('td'));
@@ -65,6 +102,37 @@ function addupdate(stud){
     td2.innerHTML= inputClass;
     td3.innerHTML= inputRoll;
     td4.innerHTML= '<i class="fa fa-edit text" onclick="update(this)" style="font-size:36px; color:#ff4500"></i> &nbsp; <i class="fa fa-trash-o text" onclick="delet(this)" style="font-size:36px;color:#dc3545"></i>';
+
+    if( inputClass == '1' ){
+        tr.style.background = '#343a40';
+      }
+      else if( inputClass == '2' ){
+        tr.style.background = "#1089ff";
+      }
+      else if( inputClass == '3' ){
+        tr.style.background = "#28a745";
+      }
+      else if( inputClass == '4' ){
+        tr.style.background = "#ffc107";
+      }
+      else if( inputClass == '5' ){
+        tr.style.background = "#fa9729";
+      }
+      else if( inputClass == '6' ){
+        tr.style.background = "#17a2b8";
+      }
+      else if( inputClass == '7' ){
+        tr.style.background = "#FFCA28";
+      }
+      else if( inputClass == '8' ){
+        tr.style.background = "#422E45";
+      }
+      else if( inputClass == '9' ){
+        tr.style.background = "#080808";
+      }
+      else if( inputClass == '10' ){
+        tr.style.background = "#874E1C";
+      }
     document.getElementById('tbl').replaceChild(tr,s);
 }
 
@@ -74,7 +142,16 @@ function delet(stud){
     s.parentNode.removeChild(s);
 }
 
+// function ch(){
+//     if(true){
 
+//         document.getElementById('tbl').style.backgroundColor ='red';
+       
+//     }
+// }
+// ch()
+
+let cor = [' blueviolet', 'brown', 'burlywood', 'chocolate', 'crimson', 'cyan', 'darkblue', 'darkgrey', 'darkred', 'darkviolet' ];
 
 
 
